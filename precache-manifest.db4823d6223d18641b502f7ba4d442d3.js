3b29c994c78172dcb8af0af46fb974711fc92367
fishr-flash <fishr.flash@gmail.com>
2020-04-22 19:23:31 +0300

    added scripts

---
+++ precache-manifest.db4823d6223d18641b502f7ba4d442d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0dcf754295fc5c3197bee637c9c0d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ccdbdc2d6bb8f757ff",
+    "url": "/static/css/main.429e6e54.chunk.css"
+  },
+  {
+    "revision": "1457ec03d274352d84ea",
+    "url": "/static/js/2.3cb247b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3cb247b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ccdbdc2d6bb8f757ff",
+    "url": "/static/js/main.e711fad3.chunk.js"
+  },
+  {
+    "revision": "41ed7f36163a1419075f",
+    "url": "/static/js/runtime-main.6ae2c8b7.js"
+  },
+  {
+    "revision": "948af06bfee6f1cd3ae27b8245180a34",
+    "url": "/static/media/img2@1x.948af06b.jpg"
+  }
+]);
